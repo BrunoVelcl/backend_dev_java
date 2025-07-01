3157e6a4997d7665a79e8c8efdc98a3470d94ac3
Bruno <brunovelcl@gmail.com>
2025-07-02 01:35:49 +0200

    refactor factoriel

--- vjezba_faktoriel/src/Main.java
+++ vjezba_faktoriel/src/Main.java
@@ -1,30 +1,23 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean run = true;
+        int input;
 
         while (run) {
             System.out.print("Unesite broj: ");
             String strInput = scanner.nextLine();
-            int input;
             try {
-                input = Integer.parseInt(strInput);
-            }catch (NumberFormatException e){
-                System.out.println(strInput + " nije valjan broj.");
-                while (!(strInput.equalsIgnoreCase("Y") | strInput.equalsIgnoreCase("N"))) {
-                    System.out.println("Želite li nastaviti unos? (Y/N)");
-                    strInput = scanner.nextLine();
-                }
-                if(strInput.equalsIgnoreCase("Y")){
-                    continue;
-                }
-                return;
+                input = inputParsing(strInput);
+            }catch (NumberNotNatural | NumberFormatException e){
+                System.out.println(e.getMessage());
+                run = continueDialog(scanner);
+                continue;
             }
+
             long result = 1;
 
             for (int i = 1; i <= input; i++) {
@@ -32,14 +25,25 @@ public class Main {
             }
 
             System.out.println("Faktoriel od: \u001b[34m" + input + " \u001b[37m je: \u001b[32m" + result + "\u001b[37m");
-            String confirmation = "";
-            while (!(confirmation.equalsIgnoreCase("Y") | confirmation.equalsIgnoreCase("N"))) {
-                System.out.print("Zelite li izracunati jos jedan? (Y/N): ");
-                confirmation = scanner.nextLine();
-            }
-            if(confirmation.equalsIgnoreCase("N")){
-                run = false;
-            }
+            run = continueDialog(scanner);
+
         }
+    }
+    private static int inputParsing(String strInput)throws NumberFormatException, NumberNotNatural{
+
+        int input = Integer.parseInt(strInput);
+        if(input < 0){
+            throw new NumberNotNatural("Broj nije prirodan.");
+        }
+        return input;
+    }
+
+    private static boolean continueDialog(Scanner scanner){
+        String confirmation = "";
+        while (!(confirmation.equalsIgnoreCase("Y") | confirmation.equalsIgnoreCase("N"))) {
+            System.out.print("Zelite li ponoviti? (Y/n): ");
+            confirmation = scanner.nextLine();
+        }
+        return !confirmation.equalsIgnoreCase("N");
     }
 }
---
+++ vjezba_faktoriel/src/NumberNotNatural.java
@@ -0,0 +1,5 @@
+public class NumberNotNatural extends RuntimeException {
+    public NumberNotNatural(String message) {
+        super(message);
+    }
+}
